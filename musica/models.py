43c313277765e38cf25fc7c7e9aa1e0d465891ac
Huelerssey Rodrigues <huelersseyrd@hotmail.com>
2022-12-27 22:14:55 -0300

    models editados

--- musica/models.py
+++ musica/models.py
@@ -1,9 +1,9 @@
-"""models from app musica"""
+"""models do app musica"""
 from django.db import models
 
 # Songs -> precisa conter a musica e o artista
 class Musica(models.Model):
-    """define songs and artist for user can insert"""
+    """ddefine musicas e artistas que um usuário pode inserir"""
     nome = models.CharField(max_length=50)
     artista = models.CharField(max_length=50)
 
@@ -12,3 +12,9 @@ class Musica(models.Model):
 
 
 # Playlist -> músicas e artistas selecionadas pelo usuário herdando os dois em uma lista
+class Playlist(models.Model):
+    """Permite ao usuário organizar as músicas e artistas"""
+    musicas = models.ForeignKey("Musica", related_name="playlist", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.musicas
